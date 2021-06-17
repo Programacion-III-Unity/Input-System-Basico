1eb39c44cd9dbbb37f9f6a8ce445a27863a9d464
Gonzalo Martinez <ggmartinez.utu@gmail.com>
2021-06-17 04:19:35 -0300

    Limpieza

--- Assets/Scripts/Gameplay/Jugador/Player.cs
+++ Assets/Scripts/Gameplay/Jugador/Player.cs
@@ -21,42 +21,29 @@ public class Player : MonoBehaviour
         entrada = GetComponent<PlayerInput>();
     }
 
-    void Update() {
-        Mover();
-    }
-
-    private void Mover() {
-        jugador.Translate(Vector3.right * this.direccionDeInput.x * Time.deltaTime * this.velocidad,Space.Self);
+    public void OnMovimiento(InputValue value)
+    {
+        this.direccionDeInput = (Vector2)value.Get();
+        jugador.Translate(Vector3.right * this.direccionDeInput.x * Time.deltaTime * this.velocidad, Space.Self);
         jugador.Translate(Vector3.up * this.direccionDeInput.y * Time.deltaTime * this.velocidad, Space.Self);
         jugador.position = new Vector3(
             ClamplearEjeX(jugador.position.x),
             ClamplearEjeY(jugador.position.y),
             jugador.position.z
         );
-
     }
 
     private float ClamplearEjeX(float posicion){
         return Mathf.Clamp(posicion, (BORDE_LATERAL * -1f), BORDE_LATERAL);
     }
-    private float ClamplearEjeY(float posicion)
-    {
+    private float ClamplearEjeY(float posicion){
         return Mathf.Clamp(posicion, (BORDE_SUPERIOR * -1f), BORDE_SUPERIOR);
     }
 
 
-    public void OnMovimiento(InputValue value)
-    {
-        Debug.Log(value.Get());
-        this.direccionDeInput = (Vector2)value.Get();
-    }
-
-    
-    public void OnDisparo(InputValue value)
-    {
+    public void OnDisparo(InputValue value){
         GameObject nuevaBala;
-        if ((float)value.Get() == 1f)
-        {
+        if ((float)value.Get() == 1f){
             nuevaBala = Instantiate(bala, jugador.position, jugador.rotation);
             nuevaBala.transform.parent = GameObject.Find("__Dynamic").transform;
         }
